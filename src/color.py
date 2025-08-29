fea81bb2146a8c33cf5182029d88978ca16b3967
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2025-08-29 14:12:05 +0300

    fix: pass correct args to hsv_to_rgb

--- src/color.py
+++ src/color.py
@@ -1,12 +1,12 @@
-from zlib import crc32
 import colorsys
+from zlib import crc32
 
 
 def pick_stable_random(to_hash: str):
     hash_value = crc32(to_hash.encode()) & 0xFFFFFFFF
-    hue = (hash_value % 100) / 100
-    value, saturation = 1, 0.75
-    r, g, b = colorsys.hsv_to_rgb(hue, value, saturation)
+    hue = (hash_value % 360) / 360
+    saturation, value = 0.75, 1.0
+    r, g, b = colorsys.hsv_to_rgb(hue, saturation, value)
     r *= 255
     g *= 255
     b *= 255
